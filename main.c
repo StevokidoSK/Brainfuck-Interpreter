379927fe3d6b8e4d728f054001b67ad0807a1029
magey3 <magey3333@gmail.com>
2020-05-29 16:50:07 +0200

    Added the input (",") instruction

--- main.c
+++ main.c
@@ -25,9 +25,10 @@ int main(){
 	for(int i = 0; i < fsize; i++){
 		if(program[i] == '+') mem[ptr] += 1;
 		if(program[i] == '-') mem[ptr] -= 1;
-		if(program[i] == '>') ptr += 1;
-		if(program[i] == '<') ptr -= 1;
+		if(program[i] == '>' && ptr != MAX_MEMORY) ptr += 1;
+		if(program[i] == '<' && ptr != 0) ptr -= 1;
 		if(program[i] == '.') printf("%c", mem[ptr]);
+		if(program[i] == ',') mem[ptr] = getchar();
 	}
 #if defined MEMORY_DUMP
 	for(int i = 0; i < MAX_MEMORY; i++) printf("%c", mem[i]);
